c0232ec8dc6d64ad19f97e95b399a3d0a28d157c
Liam Lasurdo <38768478+llasurd1@users.noreply.github.com>
2020-10-22 22:45:10 -0400

    Update clientx.cpp

--- clientx.cpp
+++ clientx.cpp
@@ -19,6 +19,7 @@ int main() {
 	clock_t start, end;
 	socklen_t len;
 	char buffer[1024];
+	buffer
 	struct sockaddr_in servaddr, cliaddr; 
 	
 	// Create a UDP socket
@@ -34,39 +35,18 @@ int main() {
 	servaddr.sin_port = htons(PORT); // port number
 	
 	//Set timeout for 1 second
-	struct timeval tv;
-	tv.tv_sec = 1;
-	tv.tv_usec = 0;
-	if(setsockopt (sockfd, SOL_SOCKET, SO_RCVTIMEO, (char *)&tv, sizeof(tv)) < 0) {
-   		 perror("Error");
-	}
 	//Keep track of number of pings
         count = 0;
-	//send 10 pings in a loop
-
 	len = sizeof(servaddr);
-	while(count<10){
-	for(int i = 0; i<10; i++){
-		//Get current time
-	        start = time(0);
-
-		//Send message to server
-		sendto(sockfd, (const char *) buffer, strlen(buffer), MSG_CONFIRM, (const struct sockaddr *) &servaddr, len);
-		//Recieve message form server
-		int n;
-	        n = recvfrom(sockfd, (char *)buffer, sizeof(buffer), MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
-		buffer[n] = '\0';
-		//See how long has passed
-		double total = (double)difftime(time(0), start);
-		//Check if ping sent for longer than a second
-		if(total >= 1.0){
-		  cout << "Connection Time out on Ping Number " << count << endl;
-		}
-		else{
-		  cout << "Ping Number " << count << "Round Trip Time " << end << " Seconds" << endl;
-		}
-		count++;
-	}
+	//Send message to server
+	sendto(sockfd, (const char *) buffer, strlen(buffer), MSG_CONFIRM, (const struct sockaddr *) &servaddr, len);
+	//Recieve message form server
+	int n;
+	n = recvfrom(sockfd, (char *)buffer, sizeof(buffer), MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
+	buffer[n] = '\0';
+	//See how long has passed		
+	cout << "Ping Number " << count << "Round Trip Time " << end << " Seconds" << endl;
+	
 	
 	}
 	return 0; 
